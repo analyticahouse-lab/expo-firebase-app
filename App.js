081e6c9d886da1ebd2e2208b3d4d752e395faedd
Berkay <analyticahouse@192.168.1.37>
2023-06-15 10:03:20 +0300

    updated

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Analytics from 'expo-firebase-analytics';
 
 export default function App() {
+
+
+  const item_jeggings = {
+    item_id: 'SKU_123',
+    item_name: 'jeggings',
+    item_category: 'pants',
+    item_variant: 'black',
+    item_brand: 'Google',
+    price: 9.99,
+    item_color: 'blue' // The item-scoped custom parameter
+  };
+
+  const params3 = {
+    currency: 'USD',
+    value: 9.99,
+    items: [item_jeggings]
+  };
+
+  const item_jeggings_quantity = {
+    ...item_jeggings,
+    quantity: 2
+  };
+
+  const params4 = {
+    currency: 'USD',
+    value: 19.98,
+    items: [item_jeggings_quantity]
+  };
+
+  const handleButtonClick = () => {
+    Analytics.logEvent('view_item', params3);
+    console.log("view_item sent", 'view_item', params3)
+  };
+  const handleAddToCart = () => {
+    Analytics.logEvent('add_to_cart', params3);
+    console.log("add_to_cart", 'view_item', params4)
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open ups App.js to start working on your app!</Text> */}
+      <div style={styles.group}>
+        <Button title="Send view_item" style={styles.button} onPress={handleButtonClick} />
+        <Button title="Send add_to_cart" style={styles.button} onPress={handleAddToCart} />
+      </div>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +61,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    marginTop: "1rem"
+  },
+  group: {
+    marginTop: "1rem",
+    display: "grid",
+    gap: "1rem"
+  }
 });
