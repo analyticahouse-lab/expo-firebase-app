57fee158972d17c7dab6998ef4d7a68054e98029
raufsamestone <berkaydemirbas@gmail.com>
2023-07-03 13:35:22 +0300

    updated

--- App.js
+++ App.js
@@ -37,17 +37,50 @@ export default function App() {
     console.log("view_item sent", 'view_item', params3)
   };
   const handleAddToCart = () => {
-    Analytics.logEvent('add_to_cart', params3);
-    console.log("add_to_cart", 'view_item', params4)
+    Analytics.logEvent('add_to_cart', params4);
+    console.log("add_to_cart sent", 'add_to_cart', params4)
   };
 
+  const handleSignUp = () => {
+    Analytics.logEvent('sign_up', {
+      method: 'Google',
+    });
+    console.log("sign_up sent")
+  };
+
+  const handleSelectContent = () => {
+    Analytics.logEvent('select_content', {
+      content_type: 'product',
+      content_id: 'C_12345',
+    });
+    console.log("select_content sent")
+  };
+
+
+  const handleGoalCompletion = () => {
+    Analytics.logEvent('goal_completion', { name: 'newsletter_submit' });
+    console.log('goal_completion', { name: 'newsletter_submit' })
+  };
+
+
+
+  const userRandomID = Math.floor(Math.random() * 100)
+
+  Analytics.setUserProperties("user_id", userRandomID);
+  // Analytics.setUserId(userRandomID);
 
   return (
     <View style={styles.container}>
       {/* <Text>Open ups App.js to start working on your app!</Text> */}
       <div style={styles.group}>
+        <Button title="Send sign_up" style={styles.button} onPress={handleSignUp} />
         <Button title="Send view_item" style={styles.button} onPress={handleButtonClick} />
+        <Button title="Send select_content" style={styles.button} onPress={handleSelectContent} />
         <Button title="Send add_to_cart" style={styles.button} onPress={handleAddToCart} />
+        <Button title="Send goal_completion" style={styles.button} onPress={handleGoalCompletion} />
+
+        <Button title="baska bisey" style={styles.button} onPress={handleGoalCompletion} />
+
       </div>
       <StatusBar style="auto" />
     </View>
